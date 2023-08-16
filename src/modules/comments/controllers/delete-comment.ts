8c38c8a28817015e90fefc6fee5f3d196568d9a9
flaming-streamz <flamingstreamz@gmail.com>
2023-08-16 18:18:11 +0300

    change: delete video route to use query params for id

--- src/modules/comments/controllers/delete-comment.ts
+++ src/modules/comments/controllers/delete-comment.ts
@@ -1,20 +1,21 @@
 import type { HTTPRequest } from "@/utils/express-callback";
+import { StatusCodes } from "http-status-codes";
 
 export default function makeDeleteComment({
   removeComment,
 }: {
   removeComment: ({ id }: { id: string }) => Promise<{ deletedCount: number }>;
 }) {
-  return async function deleteComment(httpRequest: HTTPRequest<{ id: string }>) {
+  return async function deleteComment(httpRequest: HTTPRequest<object, object, { id: string }>) {
     const headers = {
       "Content-Type": "application/json",
     };
 
-    const deletedComment = await removeComment({ id: httpRequest.params.id });
+    const deletedComment = await removeComment({ id: httpRequest.query.id });
 
     return {
       headers,
-      statusCode: deletedComment.deletedCount === 0 ? 404 : 200,
+      statusCode: deletedComment.deletedCount === 0 ? 404 : StatusCodes.NO_CONTENT,
       body: { deletedComment },
     };
   };
--- src/modules/comments/index.ts
+++ src/modules/comments/index.ts
@@ -4,6 +4,6 @@ import makeCallback from "@/utils/express-callback";
 import { deleteComment } from "./controllers";
 
 const commentsRouter = Router();
-commentsRouter.delete("/:id", makeCallback(deleteComment));
+commentsRouter.delete("/", makeCallback(deleteComment));
 
 export default commentsRouter;
